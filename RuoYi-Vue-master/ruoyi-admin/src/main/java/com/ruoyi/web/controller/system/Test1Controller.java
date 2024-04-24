77842caba00f8f373b1ce66832d0e69f3eb631be
niujiazhen <niujiazhen26@163.com>
2024-04-24 16:27:37 +0800

    测试CRUD（需改进）

---
+++ RuoYi-Vue-master/ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/Test1Controller.java
@@ -0,0 +1,98 @@
+package com.ruoyi.web.controller.system;
+
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.system.domain.Test1;
+import com.ruoyi.system.service.ITest1Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * 测试列表Controller
+ * 
+ * @author niujiazhen
+ * @date 2024-04-24
+ */
+@RestController
+@RequestMapping("/test1/test1List")
+public class Test1Controller extends BaseController
+{
+    @Autowired
+    private ITest1Service test1Service;
+
+    /**
+     * 查询测试列表列表
+     */
+    @PreAuthorize("@ss.hasPermi('test1:test1List:list')")
+    @GetMapping("/list")
+    public TableDataInfo list(Test1 test1)
+    {
+        startPage();
+        List<Test1> list = test1Service.selectTest1List(test1);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出测试列表列表
+     */
+    @PreAuthorize("@ss.hasPermi('test1:test1List:export')")
+    @Log(title = "测试列表", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, Test1 test1)
+    {
+        List<Test1> list = test1Service.selectTest1List(test1);
+        ExcelUtil<Test1> util = new ExcelUtil<Test1>(Test1.class);
+        util.exportExcel(response, list, "测试列表数据");
+    }
+
+    /**
+     * 获取测试列表详细信息
+     */
+    @PreAuthorize("@ss.hasPermi('test1:test1List:query')")
+    @GetMapping(value = "/{id}")
+    public AjaxResult getInfo(@PathVariable("id") Long id)
+    {
+        return success(test1Service.selectTest1ById(id));
+    }
+
+    /**
+     * 新增测试列表
+     */
+    @PreAuthorize("@ss.hasPermi('test1:test1List:add')")
+    @Log(title = "测试列表", businessType = BusinessType.INSERT)
+    @PostMapping
+    public AjaxResult add(@RequestBody Test1 test1)
+    {
+        return toAjax(test1Service.insertTest1(test1));
+    }
+
+    /**
+     * 修改测试列表
+     */
+    @PreAuthorize("@ss.hasPermi('test1:test1List:edit')")
+    @Log(title = "测试列表", businessType = BusinessType.UPDATE)
+    @PutMapping
+    public AjaxResult edit(@RequestBody Test1 test1)
+    {
+        return toAjax(test1Service.updateTest1(test1));
+    }
+
+    /**
+     * 删除测试列表
+     */
+    @PreAuthorize("@ss.hasPermi('test1:test1List:remove')")
+    @Log(title = "测试列表", businessType = BusinessType.DELETE)
+	@DeleteMapping("/{ids}")
+    public AjaxResult remove(@PathVariable Long[] ids)
+    {
+        return toAjax(test1Service.deleteTest1ByIds(ids));
+    }
+}
---
+++ RuoYi-Vue-master/ruoyi-system/src/main/java/com/ruoyi/system/domain/Test1.java
@@ -0,0 +1,106 @@
+package com.ruoyi.system.domain;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.ruoyi.common.annotation.Excel;
+import com.ruoyi.common.core.domain.BaseEntity;
+
+/**
+ * 测试列表对象 test1
+ * 
+ * @author niujiazhen
+ * @date 2024-04-24
+ */
+public class Test1 extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** $column.columnComment */
+    private Long id;
+
+    /** $column.columnComment */
+    @Excel(name = "${comment}", readConverterExp = "$column.readConverterExp()")
+    private String name;
+
+    /** $column.columnComment */
+    private String path;
+
+    /** $column.columnComment */
+    private Long status;
+
+    /** $column.columnComment */
+    @Excel(name = "${comment}", readConverterExp = "$column.readConverterExp()")
+    private Long userId;
+
+    /** $column.columnComment */
+    @Excel(name = "${comment}", readConverterExp = "$column.readConverterExp()")
+    private String time;
+
+    public void setId(Long id) 
+    {
+        this.id = id;
+    }
+
+    public Long getId() 
+    {
+        return id;
+    }
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+    public void setPath(String path) 
+    {
+        this.path = path;
+    }
+
+    public String getPath() 
+    {
+        return path;
+    }
+    public void setStatus(Long status) 
+    {
+        this.status = status;
+    }
+
+    public Long getStatus() 
+    {
+        return status;
+    }
+    public void setUserId(Long userId) 
+    {
+        this.userId = userId;
+    }
+
+    public Long getUserId() 
+    {
+        return userId;
+    }
+    public void setTime(String time) 
+    {
+        this.time = time;
+    }
+
+    public String getTime() 
+    {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("id", getId())
+            .append("name", getName())
+            .append("path", getPath())
+            .append("status", getStatus())
+            .append("userId", getUserId())
+            .append("time", getTime())
+            .append("remark", getRemark())
+            .toString();
+    }
+}
