c9017bd55d5d06ba878d8e2bf283744fec0c06d0
niujiazhen <niujiazhen26@163.com>
2024-06-13 13:48:03 +0800

    JavaCovergeReport

---
+++ RuoYi-Vue-master/ruoyi-system/src/main/java/com/ruoyi/system/domain/InstrumentData.java
@@ -0,0 +1,13 @@
+package com.ruoyi.system.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class InstrumentData {
+    private String code;
+    private Integer counter;
+    private String filePath;
+
+}
